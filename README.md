8768ca2d6b9e9b2e15d9f82fe598ee99bc4dbac2
Patrick Kubiak <epicpatka@gmail.com>
2024-12-09 22:22:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# patkub-site
+
+## Getting Started
+
+Node LTS 22.11.0
+
+Install dependencies
+```
+npm install
+```
+
+Build site into `dist/` folder
+```
+npm run build
+```
+
+Open `dist/index.html` in browser.
---
+++ gulpfile.babel.js
@@ -0,0 +1,108 @@
+// create require() method
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const gulp = require('gulp');
+import {deleteSync} from "del";
+const ordered = require('ordered-read-streams');
+
+// process css
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+
+// process js
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-html-minifier-terser');
+
+// static asset revisioning by appending content hash to filenames
+import rev from 'gulp-rev';
+import revRewrite from 'gulp-rev-rewrite';
+
+/**
+ * Clean dist
+ */
+gulp.task('clean', (done) => {
+    deleteSync(['dist/**'], {force:true});
+    done();
+});
+
+/**
+ * Minify JS
+ */
+gulp.task('pack-js', () => {
+    return gulp.src(['src/assets/js/main.js'])
+        .pipe(concat('main.js'))
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('dist/assets/js'));
+});
+
+/**
+ * Minify CSS
+ */
+gulp.task('pack-css', () => {    
+    return gulp.src([
+        // main css
+        'src/assets/scss/main.scss'
+    ])
+        // compile sass to css
+        .pipe(sass({ outputStyle: 'compressed' }))
+        // remove unused css
+        .pipe(purgecss({
+            content: [
+                'src/**/*.html',
+                // 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+            ]
+        }))
+        // add vendor prefixes to rules by Can I Use
+        .pipe(postcss([autoprefixer()]))
+        .pipe(gulp.dest('dist/assets/css'));
+});
+
+/**
+ * Minify HTML
+ */
+gulp.task('pack-html', () => {
+    return ordered(
+        gulp.src("src/index.html")
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('dist/')),
+        gulp.src("src/404.html")
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('dist/')),
+    );
+});
+
+/**
+ * Copy assets
+ */
+gulp.task('copy-assets', () => {
+    return ordered(
+        gulp.src("src/robots.txt")
+            .pipe(gulp.dest("dist/")),
+    );
+});
+
+/**
+ * Version and rewrite assets by appending content hash to filenames
+ * Allows for automated cache busting
+ */
+function revision() {
+    return gulp.src('dist/**/*.{css,js}')
+      .pipe(rev())
+      .pipe(gulp.src('dist/**/*.html'))
+      .pipe(revRewrite())
+      .pipe(gulp.dest('dist'));
+}
+
+/**
+ * Run everything
+ */
+gulp.task('default', gulp.series('clean', 'pack-js', 'pack-css', 'pack-html', 'copy-assets', revision));
