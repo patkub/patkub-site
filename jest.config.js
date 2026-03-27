9db6845892b64020453b709bba690c979914895f
Patrick Kubiak <epicpatka@gmail.com>
2026-03-27 11:52:29 -0400

    dependency updates

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: [
+    "<rootDir>/test/jest.polyfills.js",
     "<rootDir>/test/jest.setup.js"
-  ]
+  ],
+  transform: {
+    "^.+\\.(js|mjs)$": "babel-jest"
+  },
+  transformIgnorePatterns: []
 };
 
 export default config;
